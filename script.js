32a0b99c16b923a5871adf20af94c102ab9bd0fe
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-29 15:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateScreenSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  document.getElementById("screen-size").textContent = `Width: ${width} and Height: ${height}`;
+}
+
+updateScreenSize();
+
+window.addEventListener("resize", updateScreenSize);
